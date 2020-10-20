70e9e1d66a3a581b72a83f56734c87ff75c0864a
KatFT <71704578+KatFT@users.noreply.github.com>
2020-10-20 19:17:52 +0100

    update para fechar div e abrir outros

---
+++ js.js
@@ -0,0 +1,7 @@
+//funçao para tirar a autenticaçao e por aquele div teste
+function mostrar(){
+	document.getElementById("form").style.display="none";
+	document.getElementById("button").style.display="none";
+
+	document.getElementById("novo").style.display="block";
+}
