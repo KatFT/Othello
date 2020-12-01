39a8c12980063e83a44fded568ad986f81c1fa58
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2020-12-01 15:51:12 +0000

    FEITO

--- loader.js
+++ loader.js
@@ -1,3 +1,9 @@
+/*
+  Dezembro de 2020
+  Catarina Teixeira, up201805042
+  Cheila Alves, up201805089 
+*/
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
@@ -5,6 +11,8 @@ var startTime = Date.now();
 var cycleTime = 120000;
 var doAnim;
 
+// dirá se a animação da contagem do tempo de jogada poderá ocorrer
+// reinicia a contagem do tempo de jogada
 function canAnimate(val) {
     doAnim = val;
     startTime = Date.now();
@@ -14,7 +22,8 @@ function canAnimate(val) {
 function animate() {
 
     if (doAnim == true) {
-	
+
+	// chama recursivamente a animação
 	requestAnimationFrame(animate);
 	
 	var elapsed = Date.now() - startTime;
@@ -25,16 +34,17 @@ function animate() {
 
 	var radianRotation = Math.PI * 2 * elapsedPercent;
 
-	ctx = canvas.getContext("2d");
+	ctx = canvas.getContext("2d"); // retorna um contexto de desenho no canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
+	ctx.beginPath(); // começa um caminho ou reinicia-o
 	ctx.arc(40, 40, 30, -Math.PI / 2, -Math.PI / 2 + radianRotation, false);
 	ctx.strokeStyle = "orange";
 	ctx.lineWidth = 11.0;
-	ctx.stroke();
+	ctx.stroke(); // desenha o caminho definido pelo lineWidth()
 	
     } else {
-	
+
+	// se a animação não poder ocorrer limpa a animação
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	return;
 	
