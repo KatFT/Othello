0ea1a6447efc83904e82071e1c7cb36d92374ca4
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2020-12-26 15:16:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-let PORT = 8102;
-
 const http = require('http');
 const url = require('url');
 const path = require('path');
@@ -9,7 +7,6 @@ const fs = require('fs');
 const conf = require('./conf.js');
 const crypto = require('crypto');
 
-let game = require('./Othello1.js');
 let updater = require('./updater.js');
 
 const headers = {
@@ -75,45 +72,53 @@ function doPost(request, response) {
 
     switch(pathname) {
     case '/register':
+	// se foi efetuado um pedido com o formato incorreto, devolve erro
 	if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
 	      && Object.keys(data).length == 2)) {
-	    // ERRO
+	    answer.status = 400;
+	    break;
 	}
 
 	let nickname = data.nick;
 	let password = data.pass;
 	let found = false;
 
-	// se, anteriormente, já foi inserido um nickname nick
-	// verificar se a pass corresponde à password associada a nick
+	// a password deve ser cifrada antes de ser guardada
+	// e também antes de ser comparada uma vez que as passwords
+	// que já se encontram guardadas estão cifradas
+	const hash = crypto
+	      .createHash('md5')
+	      .update(password)
+	      .digest('hex');
+
+	// se, anteriormente, já foi inserido um nickname 'nickname'
+	// verificar se a pass corresponde à password associada a 'nickname'
 	fs.readFile('credentials.json', function(err, creds) {
 	    if (!err) { // ficheiro existe
 		
 		let users = JSON.parse(creds.toString());
-		// procura se o user com o nickname nick já foi utilizado
+		// procura se o user com o nickname 'nickname' já foi utilizado
 		for (let user in users) {
+		    
 		    if (user.nick == nickname) {
+			
 			if (user.pass == password) {
 			    answer.body = '{}';
 			    console.log('Successful login.');
 			} else {
 			    answer.body = JSON.stringfy({error: "User registered with a different password"});
-			    answer.status = 400;
+			    answer.status = 401;
 			    console.log('Bad login.');
 			}
+			
 			found = true;
 			break;
+			
 		    }
 		}
 
 		// novo user
 		if (found == false) {
-
-		    // a password deve ser cifrada antes de guardada
-		    const hash = crypto
-			  .createHash('md5')
-			  .update(password)
-			  .digest('hex');
 		    
 		    users.push({nick: nickname, pass: password});
 		    
@@ -123,18 +128,13 @@ function doPost(request, response) {
 				     if (err) throw err;
 				     console.log('New user added.');
 				 });
+		    
 		    answer.body = '{}';
 		    console.log('Successful login.');
 		    
 		}
 		
 	    } else { // se o ficheiro ainda não foi criado
-
-		// a password deve ser cifrada antes de guardada
-		const hash = crypto
-		      .createHash('md5')
-		      .update(password)
-		      .digest('hex');
 		
 		fs.writeFile('credentials.json',
 			     [{nick: nickname, pass: password}],
@@ -154,6 +154,33 @@ function doPost(request, response) {
     case '/ranking':
     case '/join':
     case '/leave':
+	// se foi efetuado um pedido com o formato incorreto, devolve erro
+	if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
+	      && data.hasOwnProperty("game") && Object.keys(data).length == 3)) {
+	    answer.status = 400;
+	    break;
+	}
+
+	let users = '';
+	fs.readFile('players.json', function(err, games) {
+	    if (!err) { // ficheiro existe
+		users = JSON.parse(games.toString());
+		users = users.filter(u => u.nick !== data.nick);
+	    } 
+	});
+
+	request.on('close', () => {
+	    console.log(data.nick + " closed connection.");
+	    fs.writeFile('players.json', users,
+			 function(err) {
+			     if (err) throw err;
+			     console.log('A connection was closed.');
+			 });
+	});
+
+	answer.body = '{}';
+	updater.update(answer);
+	
     case '/notify':
     default:
     }
