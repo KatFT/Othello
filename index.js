6dc74522f426f46249ef4c410d4b98904ca9c1e6
Cheila Alves <61353903+shay2025@users.noreply.github.com>
2020-12-28 00:10:45 +0000

    /join e /leave feitos

--- index.js
+++ index.js
@@ -13,15 +13,13 @@ const headers = {
     plain: {
         'Content-Type': 'application/javascript',
         'Cache-Control': 'no-cache',
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': 'POST'
+	'Access-Control-Allow-Origin': '*'
     },
     sse: {    
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
-		'Access-Control-Allow-Origin': '*',
-        'Connection': 'keep-alive',
-		'Access-Control-Allow-Methods': 'GET'
+	'Access-Control-Allow-Origin': '*',
+        'Connection': 'keep-alive'
     }
 };
 
@@ -31,32 +29,34 @@ http.createServer((request, response) => {
     const pathname = preq.pathname;
     console.log(pathname);
 
-	    switch(request.method) {
-		    case 'GET': // DPS TRATAR DESTE CASO PARA O UPDATE !!!
-			//answer = doGet(request, response);
-				break;
-		    case 'POST':
-			
-				var body = '';
-				
-				request.on('data', function(data) {
-				    body += data;
-				    console.log('Data: ' + body);
-				    //console.log(answer);
-				}) .on('end', async function() {
-				    // espera a promessa de uma resposta, ou seja, o tratamento do 'body'
-					    await doPost(body, response, pathname)
-						.then(res => { answer = res; })
-						.catch(console.log);
-					    handleRequest(answer, response);
-				    //console.log("HANDLE: " + answer.status);
-				})
+    switch(request.method) {
+    case 'GET': // DPS TRATAR DESTE CASO PARA O UPDATE !!!
+	//answer = doGet(request, response);
+	break;
+    case 'POST':
+	
+	var body = '';
+	
+	request.on('data', function(data) {
+	    body += data;
+	    console.log('Data: ' + body);
+	    //console.log(answer);
+	}) .on('end', async function() {
+	    // espera a promessa de uma resposta, ou seja, o tratamento do 'body'
+	    await doPost(body, response, pathname)
+		.then(res => { answer = res; })
+		.catch(console.log);
 
-				break;
-			
-		    default:
-				answer.status = 400;
-				break;
+	    console.log("POST: " + answer.body);
+	    handleRequest(answer, response);
+	    //console.log("HANDLE: " + response);
+	})
+
+	break;
+	
+    default:
+	answer.status = 400;
+	break;
     }
 
 }).listen(conf.port);
@@ -66,31 +66,18 @@ function handleRequest(request, response) {
     
     // se tudo correu bem
     if(request.status === undefined)
-		request.status = 200;
+	request.status = 200;
     // se não for um pedido do tipo GET (SSE)
     if(request.style === undefined)
-		request.style = 'plain';
+	request.style = 'plain';
 
     response.writeHead(request.status, headers[request.style]);
+    response.write(request.body);
     // método POST sem corpo de resposta
     if(request.style === 'plain')
-		response.end();
+	response.end();
     
 }
-
-// converter nomes caminhos relativos em caminhos absolutos
-function getPathname(request) {
-    // caminho relativo extraído do pedido
-    const purl = url.parse(request.url);
-    // nome caminho é normalizado para retirar .. e .
-    let pathname = path.normalize(conf.documentRoot + purl.pathname);
-
-    if (!pathname.startsWith(conf.documentRoot))
-		pathname = null;
-
-    return pathname;
-}
-
 
 // tratamento do pedido com método POST
 async function doPost(request, response, pathname) {    
@@ -98,194 +85,278 @@ async function doPost(request, response, pathname) {
     let answer = {};
 
     switch(pathname) {
-	    case '/register':
-		// se foi efetuado um pedido com o formato incorreto, devolve erro
-			if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
-			      && Object.keys(data).length == 2)) {
-			    answer.status = 400;
-			    return answer;
-			}
-			
-			let nickname = data.nick;
-			let password = data.pass;
+    case '/register':
+	// se foi efetuado um pedido com o formato incorreto, devolve erro
+	if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
+	      && Object.keys(data).length == 2)) {
+	    answer.status = 400;
+	    return answer;
+	}
+	
+	let nickname = data.nick;
+	let password = data.pass;
 
-			// a password deve ser cifrada antes de ser guardada
-			// e também antes de ser comparada uma vez que as passwords
-			// que já se encontram guardadas estão cifradas
-			const hash = crypto
-			      .createHash('md5')
-			      .update(password)
-			      .digest('hex');
+	// a password deve ser cifrada antes de ser guardada
+	// e também antes de ser comparada uma vez que as passwords
+	// que já se encontram guardadas estão cifradas
+	const passHash = crypto
+	      .createHash('md5')
+	      .update(password)
+	      .digest('hex');
 
-			// espera pela promessa que vem da verificação do login
-			// e o resultado dessa promessa ficará no 'answer'
-			await login(answer, nickname, hash)
-			    .then(res => { answer = res; })
-			    .catch(console.log);
-			
-			//console.log("ANSWER: " + answer);
+	// espera pela promessa que vem da verificação do login
+	// e o resultado dessa promessa ficará no 'answer'
+	await login(answer, nickname, passHash)
+	    .then(res => { answer = res; })
+	    .catch(console.log);
+	
+	break;
+	
+    case '/ranking':
+	
+	// como um pedido com o pathname /ranking não tem corpo
+	if(Object.keys(data).length > 0){
+	    answer.status = 400;
+	    return answer;
+	}
+	
+	// aguarda a promessa da devolução dos valores no ranking
+	await rank(answer)
+	    .then(res => { answer = res; })
+	    .catch(console.log);
+
+	break;
+
+    case '/join':
+
+	// se o corpor do pedido não estiver no formato correto então aborta
+	if (!(data.hasOwnProperty("group") && data.hasOwnProperty("nick")
+	      && data.hasOwnProperty("pass") && Object.keys(data).length == 3)) {
+	    answer.status = 400;
+	    return answer;
+	}
+
+	let group = (data.group).toString();
+
+	const groupHash = crypto
+	      .createHash('md5')
+	      .update(group)
+	      .digest('hex');
+
+	await joinGame(answer, data.nick, groupHash)
+	    .then(res => { answer = res; })
+	    .catch(console.log);
+
+	console.log(answer.body);
+	break;
+	
+    case '/leave':
+	
+	// se foi efetuado um pedido com o formato incorreto, devolve erro
+	if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
+	      && data.hasOwnProperty("game") && Object.keys(data).length == 3)) {
+	    answer.status = 400;
+	    return answer;
+	}
+
+	await logout(data.nick);
+
+	answer.body = '{}';
+	updater.update(answer.body);
+	break;
+	
+    case '/notify':
+    default:
+    }
+    return answer;    
+}
+
+async function joinGame(answer, nickname, hash) {
+    return new Promise(resolve => {
+	fs.readFile('activeGames.json', async function(err, games) {
+	    
+	    if(!err) { // se ficheiro existe
+
+		// lista de jogos ativos
+		let active = JSON.parse(games.toString());
+		// flag que indicará se o jogador encontrou outro jogador
+		// no mesmo game à espera de emparelhamento
+		let found = false; 
+
+		if(!Array.isArray(active))
+		    active = [active];
+
+		for (let i=0; i<active.length; i++) {
+		    let a = active[i];
+		    // se já houver um jogador há espera de emparelhamento com o meu hash
+		    // o jogador que entrar agora tem a cor 'light'
+		    // e insere-se o novo jogador no jogo que já está então ativo
+		    if (a.game == hash && Object.keys(a).length == 2) {
+			answer.body = JSON.stringify({game: hash, color: "light"});
+			let opponent = a.player1;
+			active.splice(i, 1);
+			active.push({game: hash, player1: opponent, player2: nickname});
+			await newGame(active);
+			found = true;
 			break;
+		    }
+		}
+
+		if (found == false) {
+		    
+		    // inserção do user na lista dos users existentes
+		    active.push({game: hash, player1: nickname});
+		    // espera da promessa que efetuará a inserção do novo game no ficheiro dos jogos ativos
+		    await newGame(active);
+		    answer.body = JSON.stringify({game: hash, color: "dark"});
+		    
+		}
+
+	    } else { // ficheiro ainda não existe
+
+		var active = [{game: hash, player1: nickname}];
+		// espera da promessa que efetuará a inserção do novo game no ficheiro dos jogos ativos
+		await newGame(active);
+		answer.body = JSON.stringify({game: hash, color: "dark"});
 		
-	    case '/ranking':
-	        //como o rank n recebe parametros se existir algo no data da erro
-	    	if(Object.keys(data).length > 0){
-	    		answer.status = 400;
-	    		return answer;
-	    	}
-	    	//funçao q vai tratar do ranking
-	    	await rank(answer)
-	    		.then(res => {answer = res;})
-	    		.catch(console.log);
-	    	
-
-	    	break;
-
-	    case '/join':
-	    case '/leave':
-			// se foi efetuado um pedido com o formato incorreto, devolve erro
-			if (!(data.hasOwnProperty("nick") && data.hasOwnProperty("pass")
-			      && data.hasOwnProperty("game") && Object.keys(data).length == 3)) {
-			    answer.status = 400;
-			    return answer;
-			}
-
-			let users = [];
-			await logout()
-			    .then(res => { users = res; })
-			    .catch(console.log)
-			
-			request.on('close', () => {
-			    console.log(data.nick + " closed connection.");
-			    fs.writeFile('players.json', users,
-					 function(err) {
-					     if (err) throw err;
-					     console.log('A connection was closed.');
-					 });
-			});
-
-			answer.body = '{}';
-			updater.update(answer);
-			break;
-			
-	    case '/notify':
-	    default:
 	    }
-	    return answer;    
+	    
+	    resolve(answer); 
+
+	});
+	
+    });
+}
+
+// regista um novo jogo no ficheiro activeGames.json
+function newGame(games) {
+    return new Promise(resolve => {
+	fs.writeFile('activeGames.json',
+		     JSON.stringify(games),
+		     function(err) {
+			 if (err) throw err;
+			 console.log('Game written in file.');
+		     });
+	resolve();
+    });
 }
 
 //retorna a promessa do ranking
-async function rank(answer){
-	return new Promise(resolve => {
-		//leitura do ficheiro que contem os dados do ranking
-		fs.readFile('ranking.json', async function(err, ranks){
-			if(!err){//ficheiro existe
-				let d=JSON.parse(ranks.toString());
-				//console.log(d);
-				//vai ordenar por ordem decerescente do nº de vitorias
-				await d.sort( function (a,b){
-					if(a.victories > b.victories) //maior para o inicio
-						return -1;
-					if(a.victories < b.victories) //menor para o fim
-						return 1;
-					return 0;
-				}); 
-				//console.log('depois: '+d);
-				// n tenho a certeza se isto e assim
-				let r= [];
-				let tam;
-				if(d.length >10)
-					tam= 10;
-				else tam = d.length;
-				for(let i=0; i<tam; i++){
-					r.push(d[i]);
-				}
+function rank(answer) {
+    return new Promise(resolve => {
+	
+	//leitura do ficheiro que contem os dados do ranking
+	fs.readFile('scores.json', async function(err, ranks){
+	    
+	    if(!err) { //ficheiro existe
+		
+		let d = JSON.parse(ranks.toString());
+		
+		//vai ordenar por ordem decerescente do nr de vitorias
+		d.sort(function (a,b) {
+		    
+		    if(a.victories > b.victories) //maior para o inicio
+			return -1;
+		    
+		    if(a.victories < b.victories) //menor para o fim
+			return 1;
+		    
+		    return 0;
+		    
+		}); 
+		
+		let r = [];
+		let tam;
+		if(d.length >10)
+		    tam = 10;
+		else tam = d.length;
+		
+		for(let i=0; i<tam; i++)
+		    r.push(d[i]);
 
-				answer.body= JSON.stringify({ranking: r});
-				console.log(answer);
+		answer.body = JSON.stringify({ranking: r});
 
-			}
+	    }
 
-			
-		});
-		//devolvido com sucesso a promessa
-		resolve(answer);
+	    //devolvido com sucesso a promessa
+	    resolve(answer);
+	    
 	});
+	
+    });
 }
-
-
-
 
 // retorna a promessa da verificação do login
 async function login(answer, nickname, hash) {
     return new Promise((resolve, reject) => {
 	// leitura do ficheiro que contém os dados de autenticação dos vários utilizadores
-		fs.readFile('credentials.json', async function(err, creds) {
+	fs.readFile('credentials.json', async function(err, creds) {
+	    
+	    if (!err) { // ficheiro existe
+		// flag utilizada para indicar se o user já alguma vez efetuou o login
+		var found = false;
+		// lista de users que já fizeram login alguma vez
+		let users = JSON.parse(creds.toString());
+		// se os dados contidos no ficheiro não estiverem em formato de array
+		// então o ficheiro só contém os dados de um só user e portanto
+		// convertemos esses dados para um array para que possamos de seguida
+		// acrescentar o user atual
+		if (!Array.isArray(users))
+		    users = [users];
+		
+		// procura se o user com o nickname 'nickname' já foi utilizado
+		for (let i=0; i<users.length; i++) {
+
+		    let user = users[i];
 		    
-		    if (!err) { // ficheiro existe
-				// flag utilizada para indicar se o user já alguma vez efetuou o login
-				var found = false;
-				// lista de users que já fizeram login alguma vez
-				let users = JSON.parse(creds.toString());
-				// se os dados contidos no ficheiro não estiverem em formato de array
-				// então o ficheiro só contém os dados de um só user e portanto
-				// convertemos esses dados para um array para que possamos de seguida
-				// acrescentar o user atual
-				if (!Array.isArray(users))
-				    users = [users];
-				
-				// procura se o user com o nickname 'nickname' já foi utilizado
-				for (let i=0; i<users.length; i++) {
+		    if (user.nick == nickname) { // se user existe no ficheiro
+			
+			if (user.pass == hash) { // dados introduzidos corretos
+			    
+			    answer.body = '{}';
+			    console.log('Successful login.');
+			    
+			} else { // dados introduzidos incorretos
+			    
+			    answer.body = JSON.stringify({error: "User registered with a different password"});
+			    answer.status = 401;
+			    console.log('Bad login.');
+			    // rejeita a promessa (é devolvido erro)
+			    reject(answer);
+			    
+			}
 
-				    let user = users[i];
-				    
-				    if (user.nick == nickname) { // se user existe no ficheiro
-					
-						if (user.pass == hash) { // dados introduzidos corretos
-						    
-						    answer.body = '{}';
-						    console.log('Successful login.');
-						    
-						} else { // dados introduzidos incorretos
-						    
-						    answer.body = JSON.stringify({error: "User registered with a different password"});
-						    answer.status = 401;
-						    console.log('Bad login.');
-						    // rejeita a promessa (é devolvido erro)
-						    reject(answer);
-						    
-						}
-
-						found = true; // o user foi encontrado
-						break;
-				    }
-				    
-				}
-
-				// novo user
-					if (found == false) {
-
-					    // inserção do user na lista dos users existentes
-					    users.push({nick: nickname, pass: hash});
-					    // espera da promessa que efetuará a inserção do novo user no ficheiro das credenciais
-					    await newLogin(users);
-					    answer.body = '{}';
-					    console.log('Successful login.');
-					    
-					}
-				
-			} else { // se o ficheiro ainda não foi criado
-
-				var users = [{nick: nickname, pass: hash}];
-				// espera da promessa que efetuará a inserção do novo user no ficheiro das credenciais
-				await newLogin(users);
-				answer.body = '{}';
-				console.log('Successful login.');
+			found = true; // o user foi encontrado
+			break;
 		    }
-
-		    // é devolvida, com sucesso, a promessa
-		    resolve(answer);
 		    
-		});
+		}
+
+		// novo user
+		if (found == false) {
+
+		    // inserção do user na lista dos users existentes
+		    users.push({nick: nickname, pass: hash});
+		    // espera da promessa que efetuará a inserção do novo user no ficheiro das credenciais
+		    await newLogin(users);
+		    answer.body = '{}';
+		    console.log('Successful login.');
+		    
+		}
+		
+	    } else { // se o ficheiro ainda não foi criado
+
+		var users = [{nick: nickname, pass: hash}];
+		// espera da promessa que efetuará a inserção do novo user no ficheiro das credenciais
+		await newLogin(users);
+		answer.body = '{}';
+		console.log('Successful login.');
+	    }
+
+	    // é devolvida, com sucesso, a promessa
+	    resolve(answer);
+	    
+	});
     });
 }
 
@@ -303,15 +374,35 @@ function newLogin(users) {
     });
 }
 
-function logout() {
+function logout(nick) {
     return new Promise((resolve, reject) => {
-		let users = [];
-		fs.readFile('players.json', function(err, games) {
-		    if (!err) { // ficheiro existe
-				users = JSON.parse(games.toString());
-				users = users.filter(u => u.nick !== data.nick);
-		    } else reject(users);
-		});
-		resolve(users);
+	let active = [];
+	
+	fs.readFile('activeGames.json', async function(err, games) {
+	    
+	    if (!err) { // ficheiro existe
+		// jogos que estão ativos de momento
+		active = JSON.parse(games.toString());
+
+		// se só tiversos um só jogo ativo
+		if(!Array.isArray(active))
+		    active = [active];
+
+		for (let i=0; i<active.length; i++) {
+		    let a = active[i];
+		    // se o jogador q tem o nickname 'nick' estiver num jogo ativo
+		    // então retira-o desse jogo ativo
+		    if (a.player1 == nick || a.player2 == nick) {
+			active.splice(i, 1);
+			await newGame(active);
+			break;
+		    }
+		}
+		
+	    } else reject();
+	    
+	});
+	
+	resolve();
     });
 }
